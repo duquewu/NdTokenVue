e6454dc540f39bafe2b1b1eefdf09cb3e03eaf3c
Wu Nan <geek5nan@gmail.com>
2020-05-27 09:27:27 +0800

    enhence: loading on demand element-ui to reduce package size for better lauching

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+// import ElementUI from 'element-ui';
+import {Button, Form, FormItem, Progress} from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI);
+// Vue.use(ElementUI);
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Progress)
 
 Vue.config.devtools
 new Vue({
